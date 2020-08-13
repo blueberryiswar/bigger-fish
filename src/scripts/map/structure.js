d9604ef4d338ac4ef28cb246497d916ae011b555
Benjamin Baer <benjamin.baer@math.uzh.ch>
2020-08-14 00:20:52 +0200

    simplified room access in structure

--- src/scripts/map/structure.js
+++ src/scripts/map/structure.js
@@ -14,6 +14,7 @@ export default class Structure {
         this.map = Array(y).fill().map(() => Array(x))
         this.defaults = defaults
         this.startingRoom = null
+        this.rooms = []
         //this.map[2][5] = this.roomFactory("Starting Room")
         this.generator = new Generator(x, y, defaults)
         this.roomLayoutSketch = this.generator.getMap()
@@ -50,26 +51,22 @@ export default class Structure {
                         } else {
                             this.map[y][x] = this.roomFactory("Room", room)
                         }
-
+                        this.rooms.push(this.map[y][x])
                     }
                 }
             }
         }
-       
+
         console.log(this.map)
     }
 
     drawMap() {
-        for (let y = 0; y < this.map.length; y++) {
-            for (let x = 0; x < this.map[y].length; x++) {
-                if (this.map[y][x] !== undefined) {
-                    for (let posY = 0; posY < this.map[y][x].roomLayout.length; posY++) {
-                        for (let posX = 0; posX < this.map[y][x].roomLayout[posY].length; posX++) {
-                            let sx = this.defaults.tileSize / 2 + this.map[y][x].x + posX * this.defaults.tileSize;
-                            let sy = this.defaults.tileSize / 2 + this.map[y][x].y + posY * this.defaults.tileSize;
-                            this.scene.add.sprite(sx, sy, this.defaults.tileSet, this.map[y][x].roomLayout[posY][posX])
-                        }
-                    }
+        for (let i = 0; i < this.rooms.length; i++) {
+            for (let posY = 0; posY < this.rooms[i].roomLayout.length; posY++) {
+                for (let posX = 0; posX < this.rooms[i].roomLayout[posY].length; posX++) {
+                    let x = this.defaults.tileSize / 2 + this.rooms[i].x + posX * this.defaults.tileSize;
+                    let y = this.defaults.tileSize / 2 + this.rooms[i].y + posY * this.defaults.tileSize;
+                    this.scene.add.sprite(x, y, this.defaults.tileSet, this.rooms[i].roomLayout[posY][posX])
                 }
             }
         }
